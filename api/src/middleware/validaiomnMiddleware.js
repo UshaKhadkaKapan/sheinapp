d31887608e97466466212a67954e2133272d6cdf
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-02 18:12:11 +1000

    complete half part of Joi

--- api/src/middleware/validaiomnMiddleware.js
+++ api/src/middleware/validaiomnMiddleware.js
@@ -3,11 +3,11 @@ import Joi from "joi";
 export const adminRegistrationValidation = (req, res, next) => {
   console.log(req.body);
   const schema = Joi.object({
-    fName: Joi.string().min(3).max(50),
-    lName: Joi.string().min(3).max(50),
-    email: Joi.string().email({ minDomainSegments: 2 }).max(50),
-    password: Joi.string().min().max(50),
+    fName: Joi.string().min(3).max(50).required(),
+    lName: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email({ minDomainSegments: 2 }).max(50).required(),
+    password: Joi.string().min().max(50).required(),
   });
+  const result = schema.validate(req.body);
+  console.log(result);
 };
-
-const result = schema.validate(req.body);
