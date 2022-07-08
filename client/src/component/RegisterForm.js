eabce11cda354ed433efbf3a74a946e119b85ddb
Usha Khadka Kapan <khadkausha83@gmail.com>
2022-07-08 17:04:34 +1000

    work on register form

--- client/src/component/RegisterForm.js
+++ client/src/component/RegisterForm.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const RegisterForm = () => {
+  const [form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
+        <h3>Register Form</h3>
+        <hr />
         <Form.Group className="mb-3 " controlId="formGroupEmail">
-          <Form.Label>First Name</Form.Label>
+          <Form.Label>fName</Form.Label>
           <Form.Control
             type="text"
             name="fName"
             placeholder="Enter your First Name"
+            required
+            onChange={handleOnChange}
           />
         </Form.Group>
         <Form.Group className="mb-3 " controlId="formGroupPassword">
-          <Form.Label>Last Name</Form.Label>
+          <Form.Label>lName</Form.Label>
           <Form.Control
             type="text"
             name="lName"
             placeholder="Enter your Last Name"
+            required
+            onChange={handleOnChange}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
@@ -27,11 +43,19 @@ const RegisterForm = () => {
             type="email"
             name="email"
             placeholder="Enter your email"
+            required
+            onChange={handleOnChange}
           />
         </Form.Group>
         <Form.Group className="mb-3 " controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder=" Enter your Password" />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder=" Enter your Password"
+            required
+            onChange={handleOnChange}
+          />
         </Form.Group>
         <Button variant="primary">Submit</Button>
       </Form>
